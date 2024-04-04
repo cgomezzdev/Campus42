80e5762e6e5537fcd483b9df42d11383a070d970
Christian Andres Gomez Zapata <cgomez-z@car1s4.42barcelona.com>
2024-04-04 21:02:15 +0200

    nothing

--- first_circle/get_next_line/get_next_line.c
+++ first_circle/get_next_line/get_next_line.c
@@ -6,19 +6,27 @@
 /*   By: cgomez-z <cgomez-z@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 17:42:55 by cgomez-z          #+#    #+#             */
-/*   Updated: 2024/04/04 18:56:56 by cgomez-z         ###   ########.fr       */
+/*   Updated: 2024/04/04 21:01:17 by cgomez-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void read_fd()
+{
+
+
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*line;
+	static t_list	*list;
 	char		*next_line;
+
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
 		return (NULL);
+	read_fd(fd,&list);
 	if (line == NULL)
 		return (NULL);
 	return (next_line);

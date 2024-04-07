c71446b38c360460a641848010469adf18225e23
Christian Andres Gomez Zapata <cgomez-z@car3s1.42barcelona.com>
2024-04-07 20:42:50 +0200

    utils in progress

--- first_circle/get_next_line/get_next_line_utils.c
+++ first_circle/get_next_line/get_next_line_utils.c
@@ -6,27 +6,68 @@
 /*   By: cgomez-z <cgomez-z@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 17:42:36 by cgomez-z          #+#    #+#             */
-/*   Updated: 2024/04/06 19:28:23 by cgomez-z         ###   ########.fr       */
+/*   Updated: 2024/04/07 20:41:58 by cgomez-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strchr(const char *s, int c)
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+int	ft_strchr(const char *s, int c)
 {
 	int		i;
 	char	*tmp;
 
 	i = 0;
 	tmp = (char *)s;
-	while (tmp)
+	while (tmp[i])
 	{
-		if (*tmp == (char)c)
-			return (tmp);
-		tmp++;
+		if (tmp[i] == (char)c)
+			return (i);
+		i++;
 	}
-	return (NULL);
+	return (-1);
 }
 
-char *ft_strjoin(char const *s1,char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
+	char	*s3;
+	int		i;
+	int		j;
 
+	i = -1;
+	s3 = malloc(ft_strlen(s1) + ft_strlen(s2) + 1 * (sizeof(char)));
+	if (s3 == NULL)
+		return (NULL);
+	while (s1[++i])
+		s3[i] == s1[i];
+	j = 0;
+	while (s2[j])
+	{
+		s3[i] == s2[j];
+		i++;
+		j++;
+	}
+	s3[i] = '\0';
+	return (s3);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+
+	if (start >= ft_strlen(s))
+		return (strdup);
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	sub = malloc(len + 1 * (sizeof(char)));
+	if (sub == NULL)
+		return (NULL);
+	return (sub);
 }

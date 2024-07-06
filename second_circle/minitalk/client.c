7991d070d1099a241d6940359dcdbf1a97bfdd31
Christian Andrés Gómez Zapata <cgomez-z@car11s2.42barcelona.com>
2024-07-06 19:34:07 +0200

    print signals

--- second_circle/minitalk/client.c
+++ second_circle/minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: cgomez-z <cgomez-z@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:13:43 by cgomez-z          #+#    #+#             */
-/*   Updated: 2024/07/04 18:24:08 by cgomez-z         ###   ########.fr       */
+/*   Updated: 2024/07/06 19:25:24 by cgomez-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,24 @@ BONUS
 El servidor confirma cada signal recibida mandando una singal al cliente.
 Soporta los caracteres Unicode.*/
 
-#include "minitalk.h"
+//#include "minitalk.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+/*void	handler_sigint(int sig)
+{
+	write(1, "sign", 4);
+}
+*/
+int	main(int ac, char *av[])
+{
+	int pid;
 
+	pid = atoi(av[1]);
+	printf("%i\n",pid);
+	kill(pid,12);
+	kill(pid,SIGUSR1);
+	return (0);
+}
--- second_circle/minitalk/server.c
+++ second_circle/minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: cgomez-z <cgomez-z@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 23:38:24 by cgomez-z          #+#    #+#             */
-/*   Updated: 2024/07/04 18:23:40 by cgomez-z         ###   ########.fr       */
+/*   Updated: 2024/07/06 19:31:04 by cgomez-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ Solo puedes utilizar estas dos signals SIGUSR1 y SIGUSR2.
 BONUS
 El servidor confirma cada signal recibida mandando una singal al cliente.
 Soporta los caracteres Unicode.*/
+//#include "minitalk.h"
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-#include "minitalk.h"
-
-void	handler_sigint(int sig)
+void	handler_sigint1(int sig)
 {
-	printf("signal: %i", sig);
+	printf("signal1: %i\n", sig);
 }
 
 int	main(void)
@@ -32,7 +35,8 @@ int	main(void)
 	pid = getpid();
 	printf("pid: %i\n", pid);
 	// si recibo señal de sigusr1 ejecuta esta funcion.
-	signal(SIGUSR1, handler_sigint);
+	signal(SIGUSR2, handler_sigint1);
+	signal(SIGUSR1, handler_sigint1);
 	while (1)
 		pause();
 	return (0);

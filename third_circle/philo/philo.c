69e055f6fa3d7b02068f59c63af5d0c2a4e77a0f
Christian <cgomezz.dev@gmail.com>
2025-04-19 02:53:45 +0200

    starting puting the time

--- third_circle/philo/philo.c
+++ third_circle/philo/philo.c
@@ -6,23 +6,37 @@
 /*   By: cgomez-z <cgomez-z@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 13:03:20 by cgomez-z          #+#    #+#             */
-/*   Updated: 2025/04/18 18:57:10 by cgomez-z         ###   ########.fr       */
+/*   Updated: 2025/04/19 02:52:41 by cgomez-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+long	get_timestamp(void)
+{
+	struct timeval	tv;
 
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+void	print_philo_status(t_philo *philo, char *accion)
+{
+	long	timestamp;
+
+	timestamp = get_timestamp() - philo->data->start_time;
+	printf("%ld philo %i %s\n", timestamp, philo->n_philo, accion);
+}
 
 void	thinking(t_philo *philo) // while try to take the forks
 {
-	printf("philo %i is thinking\n", philo->n_philo);
+	print_philo_status(philo, "is thinking");
 	usleep(philo->ttt * 1000);
 }
 
 void	sleeping(t_philo *philo) // while the time to sleep is not finish
 {
-	printf("philo %i is sleeping\n", philo->n_philo);
+	print_philo_status(philo, "is sleeping");
 	usleep(philo->tts * 1000);
 	return ;
 }
@@ -31,10 +45,10 @@ void	eating(t_philo *philo)
 // try to take the forks when he have it he eat when he finish take of the forks
 {
 	pthread_mutex_lock(philo->own_fork);
-	printf("philo %i has taken his own fork\n", philo->n_philo);
+	print_philo_status(philo, "has taken his own fork");
 	pthread_mutex_lock(philo->other_fork);
-	printf("philo %i has taken an other fork\n", philo->n_philo);
-	printf("philo %i is eating\n", philo->n_philo);
+	print_philo_status(philo, "has taken his own fork");
+	print_philo_status(philo, "is eating");
 	usleep(philo->tte * 1000);
 	pthread_mutex_unlock(philo->own_fork);
 	pthread_mutex_unlock(philo->other_fork);
@@ -44,22 +58,21 @@ void	*routine(void *arg) // call the functions eating sleeping and thinking
 {
 	t_philo *philo = (t_philo *)arg;
 
-
-	//printf("philo %i is test\n", philo->n_philo);
 	usleep(100);
-	//printf("philo %i is eating2\n", philo->n_philo);
-	if(philo->n_philo % 2 == 1)
-		usleep(philo->tte/2*1000);
+	if (philo->n_philo % 2 == 1)
+		usleep(philo->tte / 2 * 1000);
 	eating(philo);
 	sleeping(philo);
 	thinking(philo);
 	return (NULL);
 }
 
-void init_forks(t_data *data, int total)
+void	init_forks(t_data *data, int total)
 {
-	int i = 0;
-	//data->forks = malloc(sizeof(pthread_mutex_t) * total);
+	int	i;
+
+	i = 0;
+	data->forks = malloc(sizeof(pthread_mutex_t) * total);
 	while (i < total)
 	{
 		pthread_mutex_init(&data->forks[i], NULL);
@@ -72,7 +85,6 @@ void	init_philo(t_data *data, int total)
 	int	i;
 
 	i = 0;
-	
 	while (i < total)
 	{
 		pthread_create(&data->threads[i], NULL, routine, data->philos[i]);
@@ -94,21 +106,24 @@ int	main(void)
 	while (i < total_philos)
 	{
 		data.philos[i] = malloc(sizeof(t_philo));
+		data.philos[i]->data = &data;
 		data.philos[i]->n_philo = i;
-		data.philos[i]->ttt = 800;
 		data.philos[i]->tte = 200;
 		data.philos[i]->tts = 100;
+		data.philos[i]->ttt = 800;
 		data.philos[i]->own_fork = &data.forks[i];
 		data.philos[i]->other_fork = &data.forks[(i + 1) % total_philos];
 		i++;
 	}
 	init_forks(&data, total_philos);
+	data.start_time = get_timestamp();
+	printf("time %ld\n", get_timestamp() - data.start_time);
 	init_philo(&data, total_philos);
-//	i = 0;
-	/*while (i < total_philos)
+	i = 0;
+	while (i < total_philos)
 	{
 		pthread_join(data.threads[i], NULL);
 		i++;
-	}*/
+	}
 	return (0);
 }
--- third_circle/philo/philo.h
+++ third_circle/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: cgomez-z <cgomez-z@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 13:05:28 by cgomez-z          #+#    #+#             */
-/*   Updated: 2025/04/18 19:01:52 by cgomez-z         ###   ########.fr       */
+/*   Updated: 2025/04/19 02:23:11 by cgomez-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <sys/time.h>
 # include <unistd.h>
 
 typedef struct s_philo
@@ -27,7 +28,7 @@ typedef struct s_philo
 	int ttt;                     // time_to_think
 	pthread_mutex_t *own_fork;   // fork for the philo
 	pthread_mutex_t *other_fork; // fork for the other philo
-
+	struct s_data *data;         // to have acces to data like the time
 }					t_philo;
 
 typedef struct s_data
@@ -35,6 +36,7 @@ typedef struct s_data
 	t_philo			**philos;
 	pthread_t		*threads;
 	pthread_mutex_t	*forks;
+	long			start_time;
 }					t_data;
 
 #endif

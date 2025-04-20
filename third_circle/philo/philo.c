7c68c5f7885546a00e47f6743f7ad7fac3f73685
Christian <cgomezz.dev@gmail.com>
2025-04-20 03:46:07 +0200

    mutex for check the dead

--- third_circle/philo/philo.c
+++ third_circle/philo/philo.c
@@ -6,11 +6,12 @@
 /*   By: cgomez-z <cgomez-z@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 13:03:20 by cgomez-z          #+#    #+#             */
-/*   Updated: 2025/04/19 19:04:35 by cgomez-z         ###   ########.fr       */
+/*   Updated: 2025/04/20 03:38:17 by cgomez-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <string.h>
 
 long	get_timestamp(void)
 {
@@ -20,42 +21,46 @@ long	get_timestamp(void)
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
-void print_philo_status(t_philo *philo, char *accion)
+void	print_philo_status(t_philo *philo, char *accion)
 {
-    long timestamp;
+	long	timestamp;
 
-    pthread_mutex_lock(&philo->data->dead_mutex);
-    if (!philo->data->someone_die)
-    {
-        timestamp = get_timestamp() - philo->data->start_time;
-        printf("%ld philo %i %s\n", timestamp, philo->n_philo, accion);
-    }
-    pthread_mutex_unlock(&philo->data->dead_mutex);
+	pthread_mutex_lock(&philo->data->dead_mutex);
+	timestamp = get_timestamp() - philo->data->start_time;
+	// Permite imprimir si nadie ha muerto, o si el mensaje es de muerte
+	if (!philo->data->someone_die || strcmp(accion, "died!!!!!!!!!!") == 0)
+		printf("%ld philo %i %s\n", timestamp, philo->n_philo, accion);
+	pthread_mutex_unlock(&philo->data->dead_mutex);
 }
 
-
-void    check_dead(t_data *data)
+void	check_dead(t_data *data)
 {
-    int i = 0;
+	int		i;
+	long	now;
 
-    while (i < data->total_philos && data->someone_die == 0)
-    {
-        pthread_mutex_lock(&data->philos[i]->meal_mutex);
-        if (get_timestamp() - data->philos[i]->last_meal > data->philos[i]->ttd)
-        {
-            pthread_mutex_lock(&data->dead_mutex);
-            if (!data->someone_die)
-            {
-                print_philo_status(data->philos[i], "died");
-                data->someone_die = 1;
-            }
-            pthread_mutex_unlock(&data->dead_mutex);
-        }
-        pthread_mutex_unlock(&data->philos[i]->meal_mutex);
-        i++;
-    }
+	i = 0;
+	while (i < data->total_philos)
+	{
+		pthread_mutex_lock(&data->philos[i]->meal_mutex);
+		now = get_timestamp();
+		if (now - data->philos[i]->last_meal > data->philos[i]->ttd)
+		{
+			pthread_mutex_unlock(&data->philos[i]->meal_mutex);
+			pthread_mutex_lock(&data->dead_mutex);
+			if (!data->someone_die)
+			{
+				data->someone_die = 1;
+				pthread_mutex_unlock(&data->dead_mutex);
+				print_philo_status(data->philos[i], "died!!!!!!!!!!");
+				return ;
+			}
+			pthread_mutex_unlock(&data->dead_mutex);
+		}
+		else
+			pthread_mutex_unlock(&data->philos[i]->meal_mutex);
+		i++;
+	}
 }
-
 
 void	init_forks(t_data *data)
 {
@@ -93,7 +98,7 @@ int	main(void)
 	data.threads = malloc((data.total_philos) * sizeof(pthread_t));
 	data.forks = malloc((data.total_philos) * sizeof(pthread_mutex_t));
 	data.someone_die = 0;
-	pthread_mutex_init(&data.dead_mutex,NULL);
+	pthread_mutex_init(&data.dead_mutex, NULL);
 	data.start_time = get_timestamp();
 	init_forks(&data);
 	while (i < data.total_philos)
